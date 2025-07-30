e42308310e24f63c3264623a6d825f9f1601184a
aazhnaspam <aashnaspam12@gmail.com>
2025-07-31 00:44:36 +0530

    changed commit

--- frontend/movie-app/vite.config.js
+++ frontend/movie-app/vite.config.js
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-  base : './',
-=======
-  base:'',
->>>>>>> 6559e47 (changes)
 })
