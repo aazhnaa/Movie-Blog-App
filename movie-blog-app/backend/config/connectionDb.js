4a32574f062def1a66a677ef22847fb7518c81e7
Aashna Bara <aashnarose12@gmail.com>
2025-07-30 22:53:21 +0530

    Delete movie-blog-app directory

--- movie-blog-app/backend/config/connectionDb.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose")
-const connectionDb = async()=>{
-    await mongoose.connect(process.env.CONNECTION_STRING).then(()=>console.log("connection to database is successful!"))
-}
-
-module.exports=connectionDb
