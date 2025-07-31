96f796eb98a7a45c05c1c5b0599ba69cbc42f9f5
aazhnaspam <aashnaspam12@gmail.com>
2025-07-31 19:27:54 +0530

    edit package.json

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,5 +1,5 @@
-import {v2 as cloudinary} from 'cloudinary';
-import {config} from 'dotenv';
+const cloudinary = require("cloudinary").v2;
+const dotenv = require("dotenv");
 config()
 
 cloudinary.config({
